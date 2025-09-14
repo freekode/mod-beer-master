121a1625800ae5147998956e5f4fa9293fb89490
Evgeny <iam@freekode.org>
2025-09-14 20:37:42 +0200

    init

---
+++ src/items/BMTakeMeHomeItemScript.cpp
@@ -0,0 +1,22 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Config.h"
+#include "Chat.h"
+#include "../BMConfig.h"
+#include "BMTakeMeHomeItemScript.h"
+
+bool ModBeerMaster_TakeMeHome_ItemScript::OnUse(Player *player, Item *item, const SpellCastTargets &)
+{
+    if (!modBmEnabled)
+        return false;
+
+    player->TeleportTo(player->m_homebindMapId, player->m_homebindX, player->m_homebindY, player->m_homebindZ, player->GetOrientation());
+    BMSchedule(player, 1s,
+               [player, this]
+               {
+                   player->HandleEmoteCommand(EMOTE_ONESHOT_CRY);
+                   player->TextEmote("cries");
+                   ChatHandler(player->GetSession()).PSendSysMessage("Home, swweet home!");
+               });
+    return false;
+}
